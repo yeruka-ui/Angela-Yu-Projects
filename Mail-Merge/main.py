c5fb35fa322577faeefc967dc18f6d3718513193
Lauren Francisco <francisco.laurenkailey@student.auf.edu.ph>
2025-07-18 10:59:25 +0800

    Edited the invitation letters

--- Mail-Merge/main.py
+++ Mail-Merge/main.py
@@ -7,12 +7,11 @@ with open("./Input/Names/invited_names.txt") as guests:
     guest_list = [name.strip() for name in guests.readlines()]
 
 with open("./Input/Letters/starting_letter.txt", "r") as letter:
-    invitation_lines = letter.readlines()
-    invitation = "".join(invitation_lines) #Returns List as String
+    invitation_lines = letter.read()
 
 for name in guest_list:
     """Loop to create an individual letter per person. Also replaces the name for each letter to the respective person"""
-    individual_letters = invitation.replace("[name]", name)
+    individual_letters = invitation_lines.replace("[name]", name)
     with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as file:
         file.write(individual_letters)
 
