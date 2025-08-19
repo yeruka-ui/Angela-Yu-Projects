83fce80ae9d4f777d78746f839f3019081308282
Lauren <francisco.laurenkailey@auf.edu.ph>
2025-08-19 12:37:58 +0800

    Completed Quiz GUI. Learnt how to use APIs and went back to OOP.

--- GUI-Quiz-App/quiz_brain.py
+++ GUI-Quiz-App/quiz_brain.py
@@ -21,10 +21,8 @@ class QuizBrain:
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return True
+        else:
+            return False
+
--- GUI-Quiz-App/ui.py
+++ GUI-Quiz-App/ui.py
@@ -25,11 +25,11 @@ class QuizInterface:
 
         #false/true btn
         self.true_btn_img = PhotoImage(file="images/true.png")
-        self.t_btn = Button(image=self.true_btn_img, highlightthickness=0, bd=0, bg= THEME_COLOR, command= self.quiz.next_question)
+        self.t_btn = Button(image=self.true_btn_img, highlightthickness=0, bd=0, bg= THEME_COLOR, command= self.true_pressed)
         self.t_btn.grid(column=0, row=2)
 
         self.false_btn_img = PhotoImage(file="images/false.png")
-        self.f_btn = Button(image=self.false_btn_img, highlightthickness=0, bd=0, bg= THEME_COLOR)
+        self.f_btn = Button(image=self.false_btn_img, highlightthickness=0, bd=0, bg= THEME_COLOR, command= self.false_pressed)
         self.f_btn.grid(column=1, row=2)
 
         #score
@@ -49,5 +49,29 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text= q_text)
+        self.canvas.config(bg=THEME_COLOR)
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text= q_text)
+        else:
+            self.canvas.itemconfig(self.question, text= "Complete!\nWell done!")
+            self.t_btn.config(state="disabled")
+            self.f_btn.config(state="disabled")
+
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg= "#d9ffd7")
+            self.quiz.score += 1
+
+        else:
+            self.canvas.config(bg= "#ffd7d7")
+
+        self.window.after(1000, self.get_next_question)
+        self.canvas.itemconfig(self.score, text=f"Score - {self.quiz.score}")
